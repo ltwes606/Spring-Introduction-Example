7bcf9e3a7b0be846168a7e4537b68aa99e27e3c1
ltwes606 <ltwes606@gmail.com>
2023-01-01 23:03:07 +0900

    스프링 데이터 JPA 회원 리포지토리 구현

---
+++ hello-spring/src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -0,0 +1,10 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
+    Optional<Member> findByName(String name);
+}
